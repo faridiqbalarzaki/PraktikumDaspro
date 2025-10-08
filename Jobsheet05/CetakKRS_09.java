2a95a167d7c1f99011bd67fe12b1110ab38b5969
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-10-08 08:37:08 +0700

    percobaan 1 Ternary

---
+++ Jobsheet05/CetakKRS_09.java
@@ -0,0 +1,19 @@
+package Jobsheet05;
+import java.util.Scanner;
+public class CetakKRS_09 {
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas){
+            System.out.println("pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+
+        }
+        
+    }
+}
