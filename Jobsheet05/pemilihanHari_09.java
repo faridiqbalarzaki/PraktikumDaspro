fab1a8d1eb191a18dc0b12041f9785d8a9e17018
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-10-08 09:37:16 +0700

    percobaan 2

---
+++ Jobsheet05/pemilihanHari_09.java
@@ -0,0 +1,30 @@
+package Jobsheet05;
+import java.util.Scanner;
+public class pemilihanHari_09 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String  dayName, dayType;
+
+        System.out.print("Input day name: ");
+        dayName = sc.nextLine();
+
+        switch (dayName.toLowerCase()) {
+            case "monday":
+              case "tuesday":
+                case "wednesday":
+                  case "thusday":
+                    case "friday":
+                    dayType = "weekday";
+                
+                break;
+          case "saturday":
+            case "sunday":
+            dayType= "weekend";
+            break;
+            default:
+               dayType = "invalid day name";
+        }
+        System.out.println(dayName+ " is a "+dayType);
+    }
+}
