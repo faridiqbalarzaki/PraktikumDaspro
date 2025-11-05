623b3111fcf4a060114c71efd205868e298298ea
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-11-05 11:36:57 +0700

    NilaiKelompok08

---
+++ JOBSHEET8/NilaiKelompok08.java
@@ -0,0 +1,32 @@
+package JOBSHEET8;
+
+import java.util.Scanner;
+
+public class NilaiKelompok08 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        double nilai, totalNilai, rataNilai;
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai ke-" + j + " : ");
+                nilai = sc.nextDouble();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+
+            System.out.println("Rata-rata: " + rataNilai);
+
+            i++;
+        }
+
+        sc.close();
+    }
+}
