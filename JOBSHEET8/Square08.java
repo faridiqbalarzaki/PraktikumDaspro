5a1664e0934e3e5deee6865d444186fedf51cdc2
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-11-05 11:04:38 +0700

    Square08

---
+++ JOBSHEET8/Square08.java
@@ -0,0 +1,19 @@
+package JOBSHEET8;
+
+import java.util.Scanner;
+
+public class Square08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        int n = sc.nextInt();
+
+        for (int iOuter = 1; iOuter <= n; iOuter++) {
+            for (int i = 0; i <= n; i++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
