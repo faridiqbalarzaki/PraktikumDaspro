28a64ca04e0fde7a6bb9315a08e2828e956e56d3
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-11-13 20:17:09 +0700

    SearchNilai08 modif

--- JOBSHEET9/SearchNilai08.java
+++ JOBSHEET9/SearchNilai08.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class SearchNilai08 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int key = 0;
+        int hasil = -1;
+
+        System.out.print("Masukan  banyaknya nilai yang akan di input: ");
+        int nilai = sc.nextInt();
+        int[] arrNilai = new int[nilai];
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukan nilai mahasiswa ke- " + (i + 1) + " : ");
+            arrNilai[i] = sc.nextInt();
+
+        }
+        System.out.print("Masukan yang ingin di cari: ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
@@ -11,7 +24,11 @@ public class SearchNilai08 {
             }
         }
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di index ke-" + hasil);
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
         System.out.println();
     }
 }
