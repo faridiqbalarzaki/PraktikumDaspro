f30040f9888b0eae017fa1936dd3672d887b3ccf
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2025-11-24 06:36:01 +0700

    SIAKAD08

---
+++ JOBSHEET 10/SIAKAD08.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SIAKAD08 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int [][] nilai = new int[4][3];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Inputkan nilai mahasiswa ke-" + (i+1));
+            double totalPerSiswa =0;
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah "+ (j+1) + ": ") ;
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+            System.out.println("Nilai rata-rata: "+ totalPerSiswa/3);
+        }
+            System.out.println("\n=====================================");
+            System.out.println("Rata-rata Nilai setiap Mata kuliah");
+            for (int j = 0; j < 3; j++) {
+                double totalMatkul=0;
+
+                for (int i = 0; i < 4; i++) {
+                    totalMatkul += nilai[i][j];
+                    
+                }
+                System.out.println("Mata kuliah "+ (j + 1)+ ": "+ totalMatkul/4);
+            
+        }
+    }
+}
